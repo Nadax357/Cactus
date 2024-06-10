302ecc3c28e9a0b2cca7e10b4e8fb61bb1d7a75c
Joaquin Buscaglia <buscaglia.joaquin@live.com>
2024-06-10 20:03:43 -0300

    Created metrics generator

---
+++ Go Server/metrics-generator.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"math/rand"
+	"sync"
+	"time"
+)
+
+type Metrics struct {
+	CpuUsage    int `json:"cpu_usage"`
+	MemoryUsage int `json:"memory_usage"`
+}
+
+var (
+	metrics      Metrics
+	metricsMutex sync.Mutex
+)
+
+func generateMetrics() {
+	for {
+		metricsMutex.Lock()
+		metrics.CpuUsage = rand.Intn(100)
+		metrics.MemoryUsage = rand.Intn(100)
+		metricsMutex.Unlock()
+		time.Sleep(time.Second)
+	}
+}
+
+func getMetrics() Metrics {
+	metricsMutex.Lock()
+	defer metricsMutex.Unlock()
+	return metrics
+}
