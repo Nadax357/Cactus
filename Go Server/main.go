48aba422fff73894bc261ff03f2b1bcec50bd512
Joaquin Buscaglia <buscaglia.joaquin@live.com>
2024-06-10 20:11:45 -0300

    Added endpoint that returns some metrics

--- Go Server/main.go
+++ Go Server/main.go
@@ -2,15 +2,27 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
 func main() {
+	go generateMetrics()
+
 	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/metrics", metricsHandler)
 	http.ListenAndServe(":8080", nil)
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, World! This is the index")
+}
+
+func metricsHandler(w http.ResponseWriter, r *http.Request) {
+	metrics := getMetrics()
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(metrics); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
